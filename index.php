21c2df7e56f41791f86b1f8bd4152a53790bcd10
Patryk Borkowski <patrykborkowski.com@gmail.com>
2022-10-09 00:09:14 +0200

    Added MySQL database and connection. Render records to the browser.

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
+$conn = mysqli_connect('localhost', 'patryk', 'test1234', "pizza_order");
+
+if (!$conn) {
+    error_log('Connection error: ' . mysqli_connect_error());
+}
+
+// write query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at'; // ORDER BY created_at DESC
+
+// make query and get result
+$result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
+print_r($pizzas);
 
 ?>
 
@@ -7,6 +29,25 @@
 <html lang="en">
 
 <?php include('templates/header.php');  ?>
+
+<h4 class="center grey-text">Pizzas</h4>
+<div class="container">
+    <div class="row">
+        <?php foreach ($pizzas as $pizza) {  ?>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
 
 <?php include('templates/footer.php');  ?>
 
